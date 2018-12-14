b7bcd7ac06afcfab081e08cd219648f9d5cc57e1
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2018-12-14 22:22:43 +0100

    add opcode reader

--- javascript/app.js
+++ javascript/app.js
@@ -4,18 +4,25 @@ class Chip8Instance {
       console.error("Cannot instantiate 'Chip8Instance' without the 'MonitorElement' argument");
       return;
     }
-    this.decrementInterval = 1 / 60;
+    var self = this;
+    this.mainThreadSwitch = true;
+    this.FPS = 60;
+    this.timeBetweenFrame = 1 / this.FPS * 1000;
+    this.opPerCycle = 4;
+    //time 1000 to get second as miliseconds
+    this.decrementInterval = 1 / 60 * 1000;
     this.memory = new Uint8Array(4096);
     this.CPU = this.generateCPU();
     this.instructionSetArchitecture = {};
     this.monitorRes = { width: 64, height: 32 };
     this.defaultColor = ["black", "white"];
     this.monitorPixelReferences = [];
-    this.monitorTwoDimentionalMonitorArrayView = [];
+    this.twoDimentionalMonitorArrayView = [];
+    this.twoDimentionalMonitorArrayBuffer = [];
     this.monitor = null;
     this.monitor = MonitorElement;
     this.generateMonitor();
-    this.setPixelToColor(this.defaultColor[0]);
+    this.setAllPixelToColor(this.defaultColor[0]);
   }
   generateCPU() {
     return {
@@ -35,6 +42,15 @@ class Chip8Instance {
       soundCounter: 0
     }
   }
+  /**
+   * [get an opcode by reading the memory]
+   * [behavior: since the memory is segmented in block of 8 bits and an opcode 16 bits
+   * we do need to take 2 value from the memory instead of just one]
+   * @return {[Number]} [return technically an unsinedInt in 16 bits but we're in javascript soooo ;)]
+   */
+  getOpcode(){
+    return (this.memory[this.CPU.programCounter] << 8) + this.memory[this.CPU.programCounter + 1];
+  }
   generateMonitor() {
     console.log("this.monitor: ", this.monitor);
     let row = null;
@@ -44,13 +60,15 @@ class Chip8Instance {
     for (let i = 0; i < this.monitorRes.height; i++) {
       row = document.createElement("section");
       row.className = "row row_" + i;
-      this.monitorTwoDimentionalMonitorArrayView[i] = [];
+      this.twoDimentionalMonitorArrayView[i] = [];
+      this.twoDimentionalMonitorArrayBuffer[i] = [];
       for (let j = 0; j < this.monitorRes.width; j++) {
         cell = document.createElement("section");
         cell.name = "row_" + i + " cell_" + j + " pixelID_" + pixelID;
         cell.className = "cell";
         this.monitorPixelReferences.push(cell);
-        this.monitorTwoDimentionalMonitorArrayView[i].push(cell);
+        this.twoDimentionalMonitorArrayView[i].push(cell);
+        this.twoDimentionalMonitorArrayBuffer[i].push("");
         row.appendChild(cell);
         pixelID++;
       }
@@ -58,9 +76,16 @@ class Chip8Instance {
       this.monitor.appendChild(row);
     }
   }
-  setPixelToColor(color) {
+  setAllPixelToColor(color) {
     for (let i = this.monitorPixelReferences.length; i-- > 0;) {
       this.monitorPixelReferences[i].className = "cell " + color;
+    }
+  }
+  updateScreen() {
+    for (var i = this.twoDimentionalMonitorArrayView.length; i-- > 0;) {
+      for (var j = this.twoDimentionalMonitorArrayView[i].length; j-- > 0;) {
+        this.twoDimentionalMonitorArrayView[i][j].className = "cell " + this.twoDimentionalMonitorArrayBuffer[i][j];
+      }
     }
   }
   initCPU() {}
@@ -74,15 +99,40 @@ class Chip8Instance {
     let result;
     for (x = 0; x < this.monitorRes.height; x++) {
       for (y = 0; y < this.monitorRes.width; y++) {
-        result = "cell " + this.defaultColor[1];
+        result = this.defaultColor[1];
         if (y % (x + 1) === 0) {
-          result = "cell " + this.defaultColor[0];
+          result = this.defaultColor[0];
         }
-        this.monitorTwoDimentionalMonitorArrayView[x][y].className = result;
+        this.twoDimentionalMonitorArrayBuffer[x][y] = result;
       }
     }
+    this.updateScreen();
   }
-  start() {}
+  async wait(t) {
+    return await new Promise(function (resolve) {
+      setTimeout(() => {
+        console.error("time to wait: ", t);
+        resolve();
+      }, t);
+    });
+  }
+  async start() {
+    let t1, t2, t3;
+    while (this.mainThreadSwitch) {
+      t1 = performance.now();
+      //this.updateScreen();
+      t2 = performance.now();
+      await this.wait(this.timeBetweenFrame - (t2 - t1) + 1000);
+      t3 = performance.now();
+      console.error("lol", (t3 - t2), "ms");
+    }
+  }
+  stopMainThread() {
+    this.mainThreadSwitch = false;
+  }
+  mainThreadSwitchTrigger() {
+    this.mainThreadSwitch = !this.mainThreadSwitch;
+  }
 };
 
 function StartChip8() {
@@ -98,6 +148,7 @@ function start() {
   let t2 = performance.now();
   console.log("generating context took: ", t2 - t1, " ms");
   testFunction(currentInstance);
+  currentInstance.start();
 }
 
 function testFunction(chip8Instance) {
